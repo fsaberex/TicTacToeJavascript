31c13108da28964fa420e13e34d243d7c1610cbb
JeffBenton <jeffbjr56@gmail.com>
2023-10-26 23:56:52 -0400

    updated project

--- script.js
+++ script.js
@@ -1,200 +1,84 @@
 const playerOne = {
     name:"",
     num:1,
+    symbol: "X",
     occupiedCells:[],
+    Wins:0,
 }
 
 const playerTwo = {
     name:"",
     num:2,
+    symbol: "O",
     occupiedCells:[],
+    Wins:0,
 }
+
+const cells = document.querySelectorAll('[data-cell]');
+const gameMessage = document.getElementById('game-message');
+const restartButton = document.getElementById('restart-button');
+
+startGame = true;
 
 playerOne.name = prompt("Player 1 enter your name (You will be X's)", "Player 1");
 playerTwo.name = prompt("Player 2 enter your name (You will be O's)", "Player 2");
 
 let winner = false;
-const players = [playerOne, playerTwo];
-let player = players[0];
+let currentPlayer = playerOne;
 
+function handleCellClick(e) {
+    const cell = e.target;
 
-
-
-function switchToNextPlayer() {
-    if(player.num === 1){
-        alert("It's your turn " + player.name + "!");
-        document.getElementById("cell1").addEventListener("click", changeCellOneX);
-        document.getElementById("cell2").addEventListener("click", changeCellTwoX);
-        document.getElementById("cell3").addEventListener("click", changeCellThreeX);
-        document.getElementById("cell4").addEventListener("click", changeCellFourX);
-        document.getElementById("cell5").addEventListener("click", changeCellFiveX);
-        document.getElementById("cell6").addEventListener("click", changeCellSixX);
-        document.getElementById("cell7").addEventListener("click", changeCellSevenX);
-        document.getElementById("cell8").addEventListener("click", changeCellEightX);
-        document.getElementById("cell9").addEventListener("click", changeCellNineX);
-        player = players[1];
-        console.log(player);
+    if (cell.textContent !== '' || !startGame) {
+        return;
     }
-    if(player.num === 2){
-        alert("It's your turn " + player.name + "!");
-        document.getElementById("cell1").addEventListener("click", changeCellOneO);
-        document.getElementById("cell2").addEventListener("click", changeCellTwoO);
-        document.getElementById("cell3").addEventListener("click", changeCellThreeO);
-        document.getElementById("cell4").addEventListener("click", changeCellFourO);
-        document.getElementById("cell5").addEventListener("click", changeCellFiveO);
-        document.getElementById("cell6").addEventListener("click", changeCellSixO);
-        document.getElementById("cell7").addEventListener("click", changeCellSevenO);
-        document.getElementById("cell8").addEventListener("click", changeCellEightO);
-        document.getElementById("cell9").addEventListener("click", changeCellNineO);
-        player = players[0];
+
+    cell.textContent = currentPlayer.symbol;
+    cell.classList.add(currentPlayer.symbol);
+    // cell.classList.push(currentPlayer.occupiedCells);
+    console.log(cell.classList);
+    currentPlayer.occupiedCells.push(cell.classlist);
+
+    if (checkBoard()) {
+        endGame(false);
+    } else if (fullBoard()) {
+        endGame(true);
+    } else {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+        alert("It's your turn " + currentPlayer.name + "!");
     }
 }
 
-function changeCellOneX () {
-    document.getElementById("cell1").textContent = 'X';
-    players[0].occupiedCells.push("cell1");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
 
-function changeCellTwoX () {
-    document.getElementById("cell2").textContent = 'X';
-    players[0].occupiedCells.push("cell2");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellThreeX () {
-    document.getElementById("cell3").textContent = 'X';
-    players[0].occupiedCells.push("cell3");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellFourX () {
-    document.getElementById("cell4").textContent = 'X';
-    players[0].occupiedCells.push("cell4");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellFiveX () {
-    document.getElementById("cell5").textContent = 'X';
-    players[0].occupiedCells.push("cell5");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellSixX () {
-    document.getElementById("cell6").textContent = 'X';
-    players[0].occupiedCells.push("cell6");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellSevenX () {
-    document.getElementById("cell7").textContent = 'X';
-    players[0].occupiedCells.push("cell7");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellEightX () {
-    document.getElementById("cell8").textContent = 'X';
-    players[0].occupiedCells.push("cell8");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellNineX () {
-    document.getElementById("cell9").textContent = 'X';
-    players[0].occupiedCells.push("cell9");
-    checkBoard(players[0]);
-    switchToNextPlayer();
-}
-
-function changeCellOneO () {
-    document.getElementById("cell1").textContent = 'O';
-    players[1].occupiedCells.push("cell1");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellTwoO () {
-    document.getElementById("cell2").textContent = 'O';
-    players[1].occupiedCells.push("cell2");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellThreeO () {
-    document.getElementById("cell3").textContent = 'O';
-    players[1].occupiedCells.push("cell3");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellFourO () {
-    document.getElementById("cell4").textContent = 'O';
-    players[1].occupiedCells.push("cell4");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellFiveO () {
-    document.getElementById("cell5").textContent = 'O';
-    players[1].occupiedCells.push("cell5");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellSixO () {
-    document.getElementById("cell6").textContent = 'O';
-    players[1].occupiedCells.push("cell6");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellSevenO () {
-    document.getElementById("cell7").textContent = 'O';
-    players[1].occupiedCells.push("cell7");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellEightO () {
-    document.getElementById("cell8").textContent = 'O';
-    players[1].occupiedCells.push("cell8");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-function changeCellNineO () {
-    document.getElementById("cell9").textContent = 'O';
-    players[1].occupiedCells.push("cell9");
-    checkBoard(players[1]);
-    switchToNextPlayer();
-}
-
-
-function checkBoard (player) {
-    const winningCells = [["cell1","cell2","cell3"], ["cell4","cell5","cell6"], ["cell7","cell8","cell9"], ["cell1","cell4","cell7"], ["cell2","cell5","cell8"], ["cell3","cell6","cell9"], ["cell1","cell5","cell9"], ["cell3","cell5","cell7"]];
+function checkBoard () {
+    const winningCells = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
     
     for(let i=0; i < winningCells.length; i++) {
         //console.log(winningCells[i]);
         const winningCombo = winningCells[i];
         let match = 0;
 
+        // return cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[c].textContent;
+
         for (let j =0; j < winningCombo.length; j++) {
-            if (player.occupiedCells.includes(winningCombo[j])) {
+            if (currentPlayer.occupiedCells.includes(winningCombo[j])) {
                 match++;
             }
         }
 
         if (match === winningCombo.length) {
-            winner = true;
-            alert(player.name + " Wins!");
-            break;
+            return winner = true;
+            // alert(player.name + " Wins!");
+            // break;
         }
 
     }
@@ -202,15 +86,37 @@ function checkBoard (player) {
 
 }
 
-function endTurn () {
 
+function fullBoard() {
+    return [...cells].every(cell => cell.textContent !== '');
 }
+
 
 function endGame () {
+    gameStart = false;
 
+    if (draw) {
+        gameMessage.textContent = "It\'s a Draw!";
+    } 
+    if (winner === true) {
+        gameMessage.textContent = currentPlayer.name + "wins!";
+        currentPlayer.Wins = currentPlayer.Wins++;
+    }
+    //gameMessage.style.display = 'block';
 }
+
 
 function clearBoard () {
+    currentPlayer = playerOne;
+    gameStart = true;
+    //gameMessage.style.display = 'none';
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.classList.remove('X', 'O');
+    });
 
 }
 
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+restartButton.addEventListener('click', clearBoard);
